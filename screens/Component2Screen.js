77675a651ec0e2e9850e5d74a2e805795ea27906
unknown <farryvay69@gmail.com>
2019-11-07 15:14:03 +0700

    tugas

---
+++ screens/Component2Screen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const Component2Screen = () => {
+    return (
+        <View>
+            <Text> This is Component 2 Screen</Text>
+        </View>
+    );
+};
+
+const style = StyleSheet.create({
+    textStyle: {
+        fontSize: 20
+    }
+});
+
+export default Component2Screen;
